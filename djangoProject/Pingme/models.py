4e485ca9bd0dc1b3b52d16dca40ac86c990f60c3
Afreed sharief <afreed.sharief.id@gmail.com>
2020-06-12 18:41:50 +0530

    added models and registered

--- djangoProject/Pingme/models.py
+++ djangoProject/Pingme/models.py
@@ -1,21 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.core.validators import MinValueValidator, RegexValidator
 
 # Create your models here.
-# class profile(models.Model):
-#     name = models.
-#
-#     def __init__(self, coin_price=None, user=None, *args, **kwargs):
-#         if user:
-#             self.user = user
-#             qs_coin = Portfolio.objects.filter(user=self.user).values('coin').distinct()
-#             super(TransactionForm, self).__init__(self.user, *args, **kwargs)
-#             self.fields['coin'].queryset = qs_coin
-#
-#     def __init__(self, coin_price=None, user=None, *args, **kwargs):
-#         super(TransactionForm, self).__init__(*args, **kwargs)
-#         if user:
-#             self.user = user
-#             qs_coin = Portfolio.objects.filter(user=self.user).values('coin').distinct()
-#             self.fields['coin'].queryset = qs_coin
+class MyProfile(models.Model):
+    name = models.CharField(max_length = 100)
+    user = models.OneToOneField(to=User, on_delete=CASCADE)
+    age = models.IntegerField(default=18, validators=[MinValueValidator(18)])
+    address = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=20, default="single", choices=(("single","single"), ("married","married"), ("widow","widow"), ("seprated","seprated"), ("commited","commited")))
+    gender = models.CharField(max_length=20, default="female", choices=(("male","male"), ("female","female")))
+    phone_no = models.CharField(validators=[RegexValidator("^0?[5-9]{1}\d{9}$")], max_length=15, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    pic=models.ImageField(upload_to = "images\\", null=True)
+    def __str__(self):
+        return "%s" % self.user
+
+class MyPost(models.Model):
+    pic=models.ImageField(upload_to = "images\\", null=True)
+    subject = models.CharField(max_length = 200)
+    msg = models.TextField(null=True, blank=True)
+    cr_date = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(to=User, on_delete=CASCADE)
+    def __str__(self):
+        return "%s" % self.subject
 
 
+class PostComment(models.Model):
+    post = models.ForeignKey(to=MyPost, on_delete=CASCADE)
+    msg = models.TextField()
+    commented_by = models.ForeignKey(to=User, on_delete=CASCADE)
+    cr_date = models.DateTimeField(auto_now_add=True)
+    flag = models.CharField(max_length=20, null=True, blank=True, choices=(("racist","racist"), ("abbusing","abbusing")))
+    def __str__(self):
+        return "%s" % self.msg
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(to=MyPost, on_delete=CASCADE)
+    liked_by = models.ForeignKey(to=User, on_delete=CASCADE)
+    cr_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "%s" % self.liked_by
+
+
+class FollowUser(models.Model):
+    profile = models.ForeignKey(to=MyProfile, on_delete=CASCADE)
+    followed_by = models.ForeignKey(to=User, on_delete=CASCADE)
+    def __str__(self):
+        return "%s" % self.followed_by
--- djangoProject/Pingme/urls.py
+++ djangoProject/Pingme/urls.py
@@ -4,9 +4,9 @@ from django.urls.conf import include
 from Pingme import views
 from django.views.generic.base import RedirectView
 urlpatterns = [
-    path('home/', views.HomeView.as_view()),
-    path('about/', views.AboutView.as_view()),
-    path('contact/', views.ContactView.as_view()),
+    # path('home/', views.HomeView.as_view()),
+    # path('about/', views.AboutView.as_view()),
+    # path('contact/', views.ContactView.as_view()),
     path('', RedirectView.as_view(url="home/")),
 
 
